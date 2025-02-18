2c5d57ca442c1ab7740a82314100e5dd7c813bb0
Tyler Bernstein <tbernstein10@gmail.com>
2025-02-18 16:15:24 -0500

    cleanup

--- src/app/(hooks)/useSelectedTeam.tsx
+++ src/app/(hooks)/useSelectedTeam.tsx
@@ -1,28 +1,28 @@
 import { Team } from "@/types/team";
 import React, { createContext, useContext, useState, ReactNode } from "react";
 
-// Define the shape of the context value
 interface SelectedTeamContext {
   selectedTeam: Team | undefined;
   setSelectedTeam: React.Dispatch<React.SetStateAction<Team | undefined>>;
 }
 
-// Create the context with a default value
-const MyContext = createContext<SelectedTeamContext | undefined>(undefined);
+const SelectedTeamContext = createContext<SelectedTeamContext | undefined>(
+  undefined
+);
 
 export function SelectedTeamProvider({ children }: { children: ReactNode }) {
   const [selectedTeam, setSelectedTeam] = useState<Team | undefined>(undefined);
 
   return (
-    <MyContext.Provider value={{ selectedTeam, setSelectedTeam }}>
+    <SelectedTeamContext.Provider value={{ selectedTeam, setSelectedTeam }}>
       {children}
-    </MyContext.Provider>
+    </SelectedTeamContext.Provider>
   );
 }
 
 // Custom hook to use the context
 export const useSelectedTeam = (): SelectedTeamContext => {
-  const context = useContext(MyContext);
+  const context = useContext(SelectedTeamContext);
   if (!context) {
     throw new Error("useMyContext must be used within a MyContextProvider");
   }
