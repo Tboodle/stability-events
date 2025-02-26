baf22f833cdce5f418f3f523457b4e763e512c93
Tboodle <tbernstein10@gmail.com>
2025-02-26 08:10:21 -0500

    remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -224,7 +224,6 @@ export default function RootLayout({
             <NavBar />
             <main>{children}</main>
             <DropToaster />
-            <Analytics />
           </ThemeProvider>
         </SelectedTeamProvider>
         {/* <NavBar />
