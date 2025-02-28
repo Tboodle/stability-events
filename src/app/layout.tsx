7bb625635cad255436b6af80e224a32c2f366182
Tyler Bernstein <tbernstein10@gmail.com>
2025-02-28 16:09:16 -0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "./_components/theme.provider";
 import NavBar from "./_components/NavBar";
 import { FlickeringGrid } from "@/components/magicui/flickering-grid";
-import { Analytics } from "@vercel/analytics/react";
 import { SelectedTeamProvider } from "./_hooks/useSelectedTeam";
 import DropToaster from "./_components/DropToaster";
 // import * as functions from "firebase-functions";
